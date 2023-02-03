76272d6ab21972f95bdffafbc7492c22c3c8f6f3
yskanbye <yassyok@ymail.ne.jp>
2023-02-03 12:28:59 +0900

    2/3 patch

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\validation\Rule;
+use Illuminate\Validation\Rule;
 
 class UserRequest extends FormRequest
 {
